988455a4e2be041e59da7d54c432c91629dfb338
Abionics <abionics.dev@gmail.com>
2021-04-28 12:24:41 +0300

    Add reverse blind auction contract

--- contracts/BlindReverseAuction.sol
+++ contracts/BlindReverseAuction.sol
@@ -17,46 +17,33 @@ contract BlindReverseAuction is BlindAuction {
     /*
     @param value Real value of bid
     @param salt  Random value that was used to calculate hash in `calcBidHash` method
-    @value You must send all tokens of your bid, can be calculated as (bid_value + fee - deposit)
+    @value Must be enough for all gas used in this operation
     */
     function confirmBid(uint128 value, uint256 salt) override public view doUpdate inPhase(Phase.CONFIRMATION) {
-        require(msg.value + _deposit >= value + _fee, Errors.NOT_ENOUGH_TOKENS);
         uint256 hash = calcBidHash(value, salt);
         address bidAddress = _calcBidAddress(msg.sender, hash);
         BlindBid(bidAddress).confirm{
             value: 0,
-            flag: SEND_ALL_GAS,
             callback: confirmBidCallback
         }(value, msg.value);
+        msg.sender.transfer({value : 0, flag : SEND_ALL_GAS, bounce: false});
     }
 
-    function confirmBidCallback(address owner, uint256 hash, uint128 value, uint128 msgValue) public {
+    // last argument (msgValue) is unused in reverse auction
+    function confirmBidCallback(address owner, uint256 hash, uint128 value, uint128) public {
         _checkIsBidCallback(owner, hash);
         _bidsCount++;
-        uint128 leaveValue = msgValue + _deposit - value - _fee;
-        if (leaveValue > 0) {
-            owner.transfer(leaveValue);
-        }
-        if (value > _winner.value) {
-            _winner = Bid(owner, value);
-        }
+        owner.transfer(_deposit - _fee);
+        Bid bid = Bid(owner, value);
+        _updateResults(bid);
     }
 
     function _updateResults(Bid bid) override internal {
         if (_winner.owner == address(0)) {
             _winner = bid;
-        } else {
-            if (bid.value > _winner.value) {
-                _returnBid(_winner);
-                _winner = bid;
-            } else {
-                _returnBid(bid);
-            }
+        } else if (bid.value < _winner.value) {
+            _winner = bid;
         }
-    }
-
-    function _returnBid(Bid bid) private pure {
-        bid.owner.transfer(bid.value);
     }
 
 }
